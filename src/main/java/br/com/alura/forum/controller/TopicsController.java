14aa0a76c031ed9574ada58362c1e78db07e78f5
Pedro Queiroz <pedro.queiroz@thoughtworks.com>
2019-06-28 14:41:13 -0300

    Add @RestController annotation to avoid using @ResponseBody all the time

--- src/main/java/br/com/alura/forum/controller/TopicsController.java
+++ src/main/java/br/com/alura/forum/controller/TopicsController.java
@@ -2,20 +2,18 @@ package br.com.alura.forum.controller;
 
 import br.com.alura.forum.model.Course;
 import br.com.alura.forum.model.Topic;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 public class TopicsController {
 
-    public static final Topic TOPIC = new Topic("I have questions", "About Spring Boot", new Course("Spring Boot 101", "Programming"));
+    private static final Topic TOPIC = new Topic("I have questions", "About Spring Boot", new Course("Spring Boot 101", "Programming"));
 
     @RequestMapping
-    @ResponseBody
     public List<Topic> list() {
         return Arrays.asList(TOPIC, TOPIC, TOPIC);
     }
