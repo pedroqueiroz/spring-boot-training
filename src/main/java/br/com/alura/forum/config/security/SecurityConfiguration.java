57017471968f0b5869acb5c45a828d3f06135290
Pedro Queiroz <pedro.queiroz@thoughtworks.com>
2019-07-01 16:01:53 -0300

    Demand authentication for other kinds of requests

--- src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -20,7 +20,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
             .antMatchers(HttpMethod.GET, "/topics").permitAll()
-            .antMatchers(HttpMethod.GET, "/topics/*").permitAll();
+            .antMatchers(HttpMethod.GET, "/topics/*").permitAll()
+            .anyRequest().authenticated();
     }
 
     @Override
