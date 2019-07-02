819ea346a40c7aacfcc980086fd6e2a56d5e25e8
Pedro Queiroz <pedro.queiroz@thoughtworks.com>
2019-07-02 18:12:38 -0300

    Add Authorization header to Swagger ui

--- src/main/java/br/com/alura/forum/config/swagger/SwaggerConfiguration.java
+++ src/main/java/br/com/alura/forum/config/swagger/SwaggerConfiguration.java
@@ -3,10 +3,14 @@ package br.com.alura.forum.config.swagger;
 import br.com.alura.forum.model.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfiguration {
@@ -18,6 +22,16 @@ public class SwaggerConfiguration {
             .apis(RequestHandlerSelectors.basePackage("br.com.alura.forum"))
             .paths(PathSelectors.ant("/**"))
             .build()
-            .ignoredParameterTypes(User.class);
+            .ignoredParameterTypes(User.class)
+            .globalOperationParameters(
+                Collections.singletonList(new ParameterBuilder()
+                    .name("Authorization")
+                    .description("Header for JWT Token")
+                    .modelRef(new ModelRef("string"))
+                    .parameterType("header")
+                    .required(false)
+                    .build()
+                )
+            );
     }
 }
